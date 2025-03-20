32b46e4c78c4b801404f825fb5f644cf653f2fa7
FabianoPungol <fl.pungol@student.avans.nl>
2025-03-20 13:02:14 +0100

    DropdownMenu Behandeltraject Gemaakt

---
+++ ZorgApp/Assets/Codes_Scripts/BehandeltrajectSceneCode/Behandelscript.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CustomDropdown : MonoBehaviour
+{
+    public GameObject dropdownPanel; // Reference to the panel
+
+    void Start()
+    {
+        dropdownPanel.SetActive(false); // Hide panel initially
+    }
+
+    public void ToggleDropdown()
+    {
+        dropdownPanel.SetActive(!dropdownPanel.activeSelf);
+    }
+
+
+   
+}
