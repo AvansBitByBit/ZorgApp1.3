e156645249c59e3baa3ec046b46df4f1dc62a789
FabianoPungol <fl.pungol@student.avans.nl>
2025-03-22 17:01:07 +0100

    MenuScene aangepast, Hulpknoppen toegevoegd

---
+++ ZorgApp/Assets/Codes_Scripts/HulpknopCode/HulpKnopHover.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonHoverEffect : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private Vector3 originalScale;
+    public float scaleFactor = 1.2f; // How much bigger it gets
+
+    void Start()
+    {
+        originalScale = transform.localScale;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        transform.localScale = originalScale * scaleFactor;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        transform.localScale = originalScale;
+    }
+}
